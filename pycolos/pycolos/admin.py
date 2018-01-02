e5df86d5a551ef7a71b441a6459465fca0724660
Julia Lucka <julia.lucka@ocado.com>
2018-01-02 23:19:29 +0100

    Answers inlined in questions;

--- pycolos/pycolos/admin.py
+++ pycolos/pycolos/admin.py
@@ -6,7 +6,16 @@ from markdownx.admin import MarkdownxModelAdmin
 from pycolos.pycolos.models import Test, Question, Answer, TestSession, UserAnswer
 
 admin.site.register(Test)
-admin.site.register(Question, MarkdownxModelAdmin)
 admin.site.register(Answer)
 admin.site.register(TestSession)
 admin.site.register(UserAnswer)
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 1
+
+
+@admin.register(Question)
+class QuestionAdmin(MarkdownxModelAdmin):
+    inlines = [AnswerInline, ]
